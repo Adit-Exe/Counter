e1a354070f06879fe51409f5207eb6de926768da
Adit <webaxals@gmail.com>
2024-10-09 22:39:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let b = document.querySelector("#c");
+let pls = document.querySelector("#pls");
+let min = document.querySelector("#min");
+var a=0;
+b.innerText = a;
+
+
+pls.addEventListener("click", () => {
+    a++;
+    b.innerText = a;
+});
+
+
+min.addEventListener("click", () => {
+    a--;
+    b.innerText = a;
+});
