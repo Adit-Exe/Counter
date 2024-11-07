eab68d4cb6376418d8fbeb78764461743087c658
Adit <webaxals@gmail.com>
2024-11-07 09:44:09 +0530

    Added reset funtion

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let b = document.querySelector("#c");
 let pls = document.querySelector("#pls");
 let min = document.querySelector("#min");
-var a=0;
+var a = 0;
 b.innerText = a;
-
-
 pls.addEventListener("click", () => {
     a++;
     b.innerText = a;
 });
-
-
 min.addEventListener("click", () => {
     a--;
+    b.innerText = a;
+});
+
+b.addEventListener("click", () => {
+    a=0;
     b.innerText = a;
 });
