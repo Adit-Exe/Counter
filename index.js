7d850cd5c53ec5b5180fc201c73ab82d89a772a1
Adit <webaxals@gmail.com>
2024-12-17 19:26:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 let b = document.querySelector("#c");
 let pls = document.querySelector("#pls");
 let min = document.querySelector("#min");
-var a = 0;
+let div = document.querySelector("#div");
+let mul = document.querySelector("#mul");
+let mux = document.querySelector("#mux");
+
+var a=0;
 b.innerText = a;
 pls.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
     a++;
-    b.innerText = a;
+    b.innerText = a;}
 });
 min.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
     a--;
-    b.innerText = a;
+    b.innerText = a;}
+});
+mul.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
+    a=a*2;
+    b.innerText = a;}
+});
+mux.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
+    a=a*a;
+    b.innerText = a;}
+});
+div.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
+    a=a/2;
+    b.innerText = a;}
 });
 
 b.addEventListener("click", () => {
+    if (a>999999999999999999) {
+        a="∞";
+        b.innerText = a;
+    }
+    else{
     a=0;
-    b.innerText = a;
+    b.innerText = a;}
 });
+
+
